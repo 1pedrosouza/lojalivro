e4e0b796f1fb78407e976b36ace82e096d05eb99
PEDRO HENRIQUE SOUZA ALVES <pedro.hsalves2@senacsp.edu.br>
2022-03-24 11:26:27 -0300

    pagina e inicial e começo do chainsaw man

---
+++ view/chainsawman.php
@@ -0,0 +1,23 @@
+<?php
+include_once("../view/header.php")
+?>
+
+
+<div class="chainsawman">
+
+<img src="../view/img/chainsawman.webp" class="denji">
+
+
+
+<div class="titulochainsaw">
+<h1>chainsaw Man</h1>
+<h4>Fujimoto, Tatsuki</h4>
+</div>
+
+
+</div>
+
+<?php
+include_once("../view/footer.php")
+
+?>
